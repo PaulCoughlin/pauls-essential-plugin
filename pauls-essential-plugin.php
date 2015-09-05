8dc43ac9597c527b81ff41fbe65ba21c8f43da87
Paul Coughlin <paulcoughlin@users.noreply.github.com>
2015-09-05 18:18:52 +0100

    kill emoji

--- pauls-essential-plugin.php
+++ pauls-essential-plugin.php
@@ -79,3 +79,7 @@ remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'index_rel_link');
 remove_action('wp_head', 'wp_generator');
 
+// disable emoji junk..  sorry WordPress..
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' ); 
+
